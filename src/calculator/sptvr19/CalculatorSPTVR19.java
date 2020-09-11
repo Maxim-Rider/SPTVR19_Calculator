87b312dc93b3dacd2812e5bbb11ec3a8eae6d39f
Maksim Kolesnikov <maksim.kolesnikov@ivkhk.ee>
2020-09-11 09:24:28 +0300

    Заготовка проекта

---
+++ src/calculator/sptvr19/CalculatorSPTVR19.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package calculator.sptvr19;
+
+/**
+ *
+ * @author user
+ */
+public class CalculatorSPTVR19 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.printIn("-----Калькулятор-----")
+        // TODO code application logic here
+    }
+    
+}
